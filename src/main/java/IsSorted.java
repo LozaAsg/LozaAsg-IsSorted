ac0a5f274e9d10325e75c430c3bb132387ab5c23
Loza Asgedom <loza.mesfin777@gmail.com>
2023-08-13 01:28:25 +0000

    Coding Challenge: Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,13 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        int i = 0;
+
+            if(arr[i] < arr[i+1]){
+                return true;
+            }else{
+                return false;
+            }
+
     }
 }
